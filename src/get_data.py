c478366a7e20cb89adec17bcdc9de850d8b0da8d
s4shivansh29 <s4shivansh29@gmail.com>
2021-09-07 15:51:44 +0530

    update Readme.md

---
+++ src/get_data.py
@@ -0,0 +1,27 @@
+## read the parameters
+## process it
+## return the data frame
+
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):      # func for reading the configuration  file i.e. .yaml file 
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def get_data(config_path):   # fun for getting the data  and passing the configuration path and reading those config
+    config = read_params(config_path)
+    print(config)
+    data_path = config['data_source']['s3_source']
+    df = pd.read_csv(data_path, encoding = 'utf-8')
+    print(df.head())
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")  #Default coz,so that if we forget the config, it will automatically take it from params.yaml
+    parsed_args = args.parse_args()
+    get_data(config_path=parsed_args.config)
