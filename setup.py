610f84e00f5461bf957f3a511e9654e0b58737f9
s4shivansh29 <s4shivansh29@gmail.com>
2021-09-10 16:15:15 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine 0 package",
+    author="shivanshjayara",
+    package=find_packages(),
+    license="MIT"                        
+    
+)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,5 @@
 def test_generic():
-    a = 3
-    b = 3
+    a = 2
+    b = 2
         
     assert a == b
